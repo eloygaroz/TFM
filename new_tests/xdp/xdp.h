731a1447329912a59ebd8bfe2c151cd8cb5cd899
eloygaroz <voidrose@DESKTOP-JB9FQR6.>
2024-03-21 12:32:06 +0100

    Fixed incorrect declaration

--- new_tests/xdp/xdp.h
+++ new_tests/xdp/xdp.h
@@ -3,7 +3,7 @@ struct event {
     int meta_size;        // Size of metadata
     int ingress_ifindex;  // Ingress interface index
     int pid;              // Process ID
-    char comm[TASK_COMM_LEN]; // Command name of the process
+    char comm[16]; // Command name of the process
     int cpu_core;         // CPU core identifier
     //int egress_ifindex;
 };
