8e2a5d6e754961a2533a495854b5cc04b86e2bf4
eduardo-hornos <eduardo.hornosm@masmovil.com>
2024-04-08 16:22:42 +0200

    new api vertx example

--- resources/api-vertx/src/main/java/com/api/handler/HealthCheckRestHandlerImpl.java
+++ resources/api-vertx/src/main/java/com/api/handler/HealthCheckRestHandlerImpl.java
@@ -2,6 +2,8 @@ package com.api.handler;
 
 
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.Json;
 import io.vertx.rxjava3.core.http.HttpServerResponse;
 import io.vertx.rxjava3.ext.web.Router;
@@ -10,6 +12,7 @@ import java.util.Map;
 
 public class HealthCheckRestHandlerImpl implements HealthCheckRestHandler {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheckRestHandlerImpl.class.getName());
 
   @Override
   public void addHandlersTo(Router router) {
@@ -19,9 +22,12 @@ public class HealthCheckRestHandlerImpl implements HealthCheckRestHandler {
   @Override
   public void check(RoutingContext context) {
     HttpServerResponse response = context.response();
+    LOGGER.info(String.format("Inbound request: %s", context.request().path()));
     if (!response.ended() && !response.closed()) {
+      var body = Map.of("message","ok");
+      LOGGER.info(String.format("Response: %s", body));
       response.setStatusCode(HttpResponseStatus.OK.code()).putHeader("Content-Type", "application/json")
-          .end(Json.encodePrettily(Map.of("message","ok")));
+          .end(Json.encodePrettily(body));
     }
   }
 }
